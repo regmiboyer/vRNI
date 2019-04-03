317d7091927fe1aefaea1d4f634320957faa8f4a
Shisir Regmi <regmiboyer@outlook.com>
2019-04-03 11:28:50 +1100

    Add files via upload

---
+++ lib/vrni.login.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+read -r -d '' BODY <<-CONFIG
+{
+	"username": "demouser@cmbu.local",
+	"password": "demoVMware1!",
+	"domain": {
+		"domain_type": "LOCAL"
+	}
+}
+CONFIG
+
+URL='https://1.1.1.1/api/ni/auth/token'
+
+RESPONSE=$(curl -k -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' \
+	-d "${BODY}" \
+"${URL}" 2>/dev/null)
+
+printf "${RESPONSE}" | jq --tab . 1>&2
+printf "${RESPONSE}" | jq -r '.token' > vrni.token.txt
