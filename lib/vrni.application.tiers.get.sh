8d530e6bb86354b849f5a5184a8c8bb505fedefb
Shisir Regmi <regmiboyer@outlook.com>
2019-04-05 10:58:47 +1100

    Update vrni.application.tiers.get.sh

--- lib/vrni.application.tiers.get.sh
+++ lib/vrni.application.tiers.get.sh
@@ -2,7 +2,7 @@
 
 APPID="${1}"
 TOKEN=$(cat vrni.token.txt)
-URL="https://10.196.164.25/api/ni/groups/applications/${APPID}/tiers"
+URL="https://vRNI/api/ni/groups/applications/${APPID}/tiers"
 
 RESPONSE=$(curl -k -X GET --header 'Accept: application/json' \
 	--header "Authorization: NetworkInsight ${TOKEN}" \
